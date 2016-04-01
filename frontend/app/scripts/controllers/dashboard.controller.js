aea718720753d493b5671c7387cf29445e3417c9
Clovis Lemes Ferreira Junior <clovis.ferreira@serpro.gov.br>
2016-04-01 17:26:23 -0300

    Remoção da implementação do Dashboard

--- frontend/app/scripts/controllers/dashboard.controller.js
+++ frontend/app/scripts/controllers/dashboard.controller.js
@@ -7,17 +7,6 @@
 
 	function DashboardController($scope, DashboardService) {
 
-		$scope.userNumber = 0;
-		$scope.bookNumber = 0;
-
-		DashboardService.getTotalUsers().then(function(total){
-			$scope.userNumber = total;
-		});
-
-		DashboardService.getTotalBooks().then(function(total){
-			$scope.bookNumber = total;
-		});
-
     }
 
 })();
